2a6b890503e53ef15f16e9be49ba634170a4152c
Ryu Jae Wook <Ryu Jae Wook@RyuJaeWook-PC>
2014-04-15 02:15:07 +0900

    새벽2시14분 장바구니 통합 해야될일 : 장바구니에 결제에 필요한 파라미터 변수 넘기고, 장바구니에 결제하기 버튼 추가 시키고, readypayment.jsp에서 변경된 결제모듈 받기.

---
+++ src/board/rest/action/ListCartAction.java
@@ -0,0 +1,37 @@
+package board.rest.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import board.cart.dto.CartDTO;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.opensymphony.xwork2.Action;
+
+import common.ConDAOAware;
+
+public class ListCartAction implements Action, ConDAOAware
+{
+    private SqlMapClient sqlMapper;
+    private List<CartDTO> list = new ArrayList<CartDTO>();
+    
+    //ConDAOAware 인터페이스의 메서드(인터셉터에서 호출)
+    public void setConDAO(SqlMapClient sqlMapper) { 
+        this.sqlMapper = sqlMapper;
+    }    
+    
+    public String execute() throws Exception {
+        list = sqlMapper.queryForList("Rest.selectCartAll");
+        
+        return SUCCESS;
+    }
+
+    public List<CartDTO> getList(){
+        return list;
+    }
+
+    public void setList(List<CartDTO> list){
+        this.list = list;
+    }
+
+}
