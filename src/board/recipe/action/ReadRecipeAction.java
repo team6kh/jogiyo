f9c0d44ed47f7f5f92ed573d7ceec7d40b4f7043
knkim0120 <kn-kim0120@hanmail.net>
2014-04-25 09:58:24 +0900

    04.25

--- src/board/recipe/action/ReadRecipeAction.java
+++ src/board/recipe/action/ReadRecipeAction.java
@@ -1,10 +1,7 @@
 package board.recipe.action;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URLEncoder;
-
 
 import board.recipe.dto.RecipeCommandDTO;
 import board.recipe.dto.RecipeDTO;
@@ -18,16 +15,15 @@ public class ReadRecipeAction extends ActionSupport implements ConDAOAware {
 
 	public static SqlMapClient sqlMapper;
 
-	private int isrecommand =1;
+	private int isrecommand = 1;
 	private RecipeDTO paramClass = new RecipeDTO(); // 파라미터를 저장할 객체
 	private RecipeDTO resultClass = new RecipeDTO(); // 쿼리 결과 값을 저장할 객체
 	private RecipeCommandDTO paramRC = new RecipeCommandDTO();
-	private RecipeCommandDTO resultRC = new RecipeCommandDTO(); 
+	private RecipeCommandDTO resultRC = new RecipeCommandDTO();
 	private String session_id;
 	private int currentPage;
 	private int recipe_num;
 	private String recipe_password;
-	private String fileUploadPath = "D:\\김경남\\Java\\upload\\";
 	private InputStream inputStream;
 	private String contentDisposition;
 	private long contentLength;
@@ -53,49 +49,30 @@ public class ReadRecipeAction extends ActionSupport implements ConDAOAware {
 		return SUCCESS;
 	}
 
-	// 첨부 파일 다운로드
-	public String download() throws Exception {
-
-		// 해당 번호의 파일 정보를 가져온다.
-		resultClass = (RecipeDTO) sqlMapper.queryForObject("selectOne",
-				getRecipe_num());
-
-		// 파일 경로와 파일명을 file 객체에 넣는다.
-		File fileInfo = new File(fileUploadPath + resultClass.getRecipe_file());
-
-		// 다운로드 파일 정보 설정.
-		setContentLength(fileInfo.length());
-		setContentDisposition("attachment;filename="
-				+ URLEncoder.encode(resultClass.getRecipe_orgfile(), "UTF-8"));
-		setInputStream(new FileInputStream(fileUploadPath
-				+ resultClass.getRecipe_file()));
-
-		return SUCCESS;
-	}
-
 	// recommandAction
 	public String recommandAction() throws Exception {
-        
-		//해당글에 추천 유무 확인
-        paramClass.setRecipe_num(getRecipe_num());
+
+		// 해당글에 추천 유무 확인
+		paramClass.setRecipe_num(getRecipe_num());
 		paramRC.setRecipeCommand_num(getRecipe_num());
 		paramRC.setRecipeCommand_writer(getSession_id());
-		
-		resultRC = (RecipeCommandDTO) sqlMapper.queryForObject("Recipe.commandID", paramRC);
-		if(resultRC == null){
+
+		resultRC = (RecipeCommandDTO) sqlMapper.queryForObject(
+				"Recipe.commandID", paramRC);
+		if (resultRC == null) {
 			System.out.println("rdDTO is null!");
 			// 해당 글의 추천수 +1.
 			sqlMapper.insert("Recipe.insertRecipeCommand", paramRC);
 			sqlMapper.update("Recipe.updateRecommand", paramClass);
 
 			// 해당 번호의 글을 가져온다.
-			resultClass = (RecipeDTO) sqlMapper.queryForObject("Recipe.selectOne",
-					getRecipe_num());
+			resultClass = (RecipeDTO) sqlMapper.queryForObject(
+					"Recipe.selectOne", getRecipe_num());
 			isrecommand = 0;
-		}else{
-			isrecommand =1;
+		} else {
+			isrecommand = 1;
 		}
-		
+
 		return SUCCESS;
 
 	}
@@ -122,14 +99,6 @@ public class ReadRecipeAction extends ActionSupport implements ConDAOAware {
 			return ERROR;
 
 		return SUCCESS;
-	}
-
-	public String getFileUploadPath() {
-		return fileUploadPath;
-	}
-
-	public void setFileUploadPath(String fileUploadPath) {
-		this.fileUploadPath = fileUploadPath;
 	}
 
 	public RecipeDTO getParamClass() {
@@ -204,7 +173,6 @@ public class ReadRecipeAction extends ActionSupport implements ConDAOAware {
 		this.pagingHtml = pagingHtml;
 	}
 
-	
 	public String getSession_id() {
 		return session_id;
 	}
@@ -220,11 +188,5 @@ public class ReadRecipeAction extends ActionSupport implements ConDAOAware {
 	public void setIsrecommand(int isrecommand) {
 		this.isrecommand = isrecommand;
 	}
-	
-	
-
-	
-	
-	
 
 }
---
+++ src/board/recipe/dto/RecipeCommandDTO.java
@@ -0,0 +1,21 @@
+package board.recipe.dto;
+
+public class RecipeCommandDTO {
+	private int recipeCommand_num;
+	private String recipeCommand_writer;
+	public int getRecipeCommand_num() {
+		return recipeCommand_num;
+	}
+	public void setRecipeCommand_num(int recipeCommand_num) {
+		this.recipeCommand_num = recipeCommand_num;
+	}
+	public String getRecipeCommand_writer() {
+		return recipeCommand_writer;
+	}
+	public void setRecipeCommand_writer(String recipeCommand_writer) {
+		this.recipeCommand_writer = recipeCommand_writer;
+	}
+	
+	
+
+}
