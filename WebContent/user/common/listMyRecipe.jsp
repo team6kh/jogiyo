a266c0f5ebf846b92b3fbd1800d0ebf8ed79faf3
knkim0120 <kn-kim0120@hanmail.net>
2014-05-02 12:03:37 +0900

    마이레시피 검색 기능 추가

--- WebContent/user/common/listMyRecipe.jsp
+++ WebContent/user/common/listMyRecipe.jsp
@@ -178,7 +178,72 @@
 		<!-- /페이징 -->
 
 				<!-- 버튼 -->
+				<div class="col-md-12">
+			<div class="form-inline pull-right">
+				<!-- 검색[선택] -->
+				<select class="form-control" name="recipe_search_target" id="recipe_search_target" title="검색" onchange="detailsearch()">
+					<option value="null">검색[선택]</option>
+					<option value="recipe_detailsearch">상세검색</option>
+				</select>
+				<!-- /.검색[선택] -->
+				</div>
+				</div>
 				<!-- /.버튼 -->
+				
+				<br />
+		<br />
+
+		<!-- 상세검색 폼 -->
+		<form name="recipe_search" action="myrecipe_search.action" enctype="multipart/form-data">
+
+			<!-- 상세검색 시에 나타난다. -->
+			<div id="detailsearch" style="display: none">
+				<div class="col-md-12">
+					<table class="table table-condensed">
+						<tr>
+							<td class="text-center">종류</td>
+							<td>
+								<select name="recipe_foodkind" style="width: 120px" id="recipe_foodkind">
+									<option value="">선택하세요</option>
+									<option value="한식">한식</option>
+									<option value="중식">중식</option>
+									<option value="일식">일식</option>
+									<option value="양식">양식</option>
+									<option value="기타">기타</option>
+								</select>
+							</td>
+							
+							<td><input type="hidden" name="recipe_writerinput" value="${session_id }"></td>
+						</tr>
+						<tr>
+							<td class="text-center">요리명</td>
+							<td colspan="3"><input type="text" name="recipe_foodnameinput"></td>
+						</tr>
+						<tr>
+							<td class="text-center">제목+내용</td>
+							<td colspan="3"><input type="text" name="recipe_subjectinput"></td>
+						</tr>
+						<tr>
+							<td class="text-center">소요시간</td>
+							<td colspan="3">
+								<input type="text" name="recipe_timeinput1" size="5">&nbsp;~&nbsp;<input type="text" name="recipe_timeinput2" size="5">
+							</td>
+						</tr>
+						<tr>
+							<td class="text-center">비용</td>
+							<td colspan="3">
+								<input type="text" name="recipe_priceinput1" size="5">&nbsp;~&nbsp;<input type="text" name="recipe_priceinput2" size="5">
+							</td>
+						</tr>
+						<tr>
+							<td class="text-right" colspan="4">
+								<input type="reset" class="btn btn-default" value="초기화" />&nbsp;<input type="submit" class="btn btn-primary" value="검색">
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<!--/.상세검색 시에 나타난다. -->
 			
 			</div>
 			<!-- /.main -->
--- src/board/recipe/action/SearchRecipeAction.java
+++ src/board/recipe/action/SearchRecipeAction.java
@@ -28,6 +28,8 @@ public class SearchRecipeAction extends ActionSupport implements ConDAOAware,Pre
     private PagingAction page;  //페이징 클래스
     
     private String actionName = "searchRecipe" ; //페이지 액션과 로그인 액션에서 쓰인다.
+    private String myListactionName = "listMyRecipe" ;
+    private String session_id;
 
 
    
@@ -48,6 +50,37 @@ public class SearchRecipeAction extends ActionSupport implements ConDAOAware,Pre
     {
         paramClass = new RecipeSearchDTO();
         
+    }
+    
+    public String myListSearch() throws Exception
+    {
+    	 if(paramClass.getRecipe_priceinput1()==0 && paramClass.getRecipe_priceinput2()==0 && paramClass.getRecipe_timeinput1()==0 && paramClass.getRecipe_timeinput2()==0){
+         	list = sqlMapper.queryForList("Recipe.detailSearchRecipeEmpty", paramClass);
+         }else if(paramClass.getRecipe_timeinput1()==0 && paramClass.getRecipe_timeinput2()==0){
+         	list = sqlMapper.queryForList("Recipe.detailSearchRecipePrice", paramClass);
+         }else if(paramClass.getRecipe_priceinput1()==0 && paramClass.getRecipe_priceinput2()==0){
+         	list = sqlMapper.queryForList("Recipe.detailSearchRecipeTime", paramClass);
+         }else{//모두 기입했을때
+         	list = sqlMapper.queryForList("Recipe.detailSearchRecipeAll", paramClass);
+         }
+         
+         totalCount = list.size(); //전체 글 갯수를 구한다.
+         page = new PagingAction(myListactionName, currentPage, totalCount, blockCount, blockPage, session_id); //PagingAction 객체 생성
+         pagingHtml = page.getPagingHtml().toString();  //페이지 HTML 생성.
+         
+         // 현재 페이지에서 보여줄 마지막 글의 번호 설정.
+                 int lastCount = totalCount;
+
+                 // 현재 페이지의 마지막 글의 번호가 전체의 마지막 글 번호보다 작으면 lastCount를 +1 번호로 설정.
+                 if (page.getEndCount() < totalCount)
+                     lastCount = page.getEndCount() + 1;
+
+                 // 전체 리스트에서 현재 페이지만큼의 리스트만 가져온다.
+                 list = list.subList(page.getStartCount(), lastCount);
+                
+         return SUCCESS;
+
+    	
     }
 
 
@@ -140,5 +173,15 @@ public class SearchRecipeAction extends ActionSupport implements ConDAOAware,Pre
 	public void setList(List<RecipeDTO> list) {
 		this.list = list;
 	}
+
+	public String getSession_id() {
+		return session_id;
+	}
+
+	public void setSession_id(String session_id) {
+		this.session_id = session_id;
+	}
+	
+	
    
 }
